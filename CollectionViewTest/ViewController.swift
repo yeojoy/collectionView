e13b1b691f99d76215d5a56e8f0de6ee6c5994ed
yeojoy <yeojoy.kim@gmail.com>
2017-12-12 01:43:55 +0900

    Initial Commit

---
+++ CollectionViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by Yeojong Kim on 2017. 12. 12..
+//  Copyright © 2017년 Yeojong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
